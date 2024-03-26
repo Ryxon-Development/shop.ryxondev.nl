ea2e20e3e6565452ad43e22f88b7d1f2b4305ac6
Ryan Hendriks <ryanhendriks@live.nl>
2024-03-26 17:41:14 +0100

    - button and colorpickers added for headers and logos.

--- wp-content/themes/ryxon-shop/inc/widgets/pricetable/pricetable.php
+++ wp-content/themes/ryxon-shop/inc/widgets/pricetable/pricetable.php
@@ -5,9 +5,9 @@ class Price_Table_Widget extends WC_Widget {
 
     function __construct() {
         $this->widget_cssclass    = 'ryxon_widget';
-        $this->widget_description = __('Displays customizable content.', 'best-shop');
+        $this->widget_description = __('Displays price information!', 'best-shop');
         $this->widget_id          = 'ryxon_widget_id';
-        $this->widget_name        = __('Ryxon Widget 1.1', 'best-shop');
+        $this->widget_name        = __('Price Table Pro', 'best-shop');
         parent::__construct();
     }
 
@@ -16,40 +16,64 @@ class Price_Table_Widget extends WC_Widget {
         $properties = !empty($instance['properties']) ? $instance['properties'] : [];
         $title = !empty($instance['title']) ? $instance['title'] : '';
         $description = !empty($instance['description']) ? $instance['description'] : '';
+        $button_txt = !empty($instance['button_txt']) ? $instance['button_txt'] : '';
+        $button_url = !empty($instance['button_url']) ? $instance['button_url'] : '';
+        $color = !empty($instance['color']) ? $instance['color'] : '';
         ?>
 
         <div class='textwidget' style='width:100%;'>
-            <h1>Ryxondev widget 1.1</h1>
-            <p>Title: <?= $title ?></p>
-            <p>Description: <?= $description ?></p>
-            <?php if (!empty($properties)) :
+            <div class="heading" style="background: <?= $color; ?>">
+                <h2><?= $title ?></h2>
+                <p><?= $description ?></p>
+            </div>
 
-                var_dump($properties);
-                ?>
-                <h3>Properties:</h3>
+            <div class="elementor-price-table__price price">
+                <span class="elementor-price-table__currency">€</span>
+                <span class="elementor-price-table__integer-part"> <?= $instance['price']?> </span>
+                <span class="elementor-price-table__period elementor-typo-excluded"><?= $instance['price_txt'] ?></span>
+            </div>
+
+            <div class="props">
+            <?php if (!empty($properties)) : ?>
                 <ul style="list-style: none; padding: 0;">
                     <?php foreach ($properties as $property) : ?>
-                        <li style="padding-left: 20px; position: relative; display: flex; align-items: center;">
-                            <i style="position: absolute; left: 0;" class="fa fa-<?= $property['icon'] ?>"></i>
+                        <li>
+                            <i style="color: <?=$property['color']?>" class="fa fa-<?= $property['icon'] ?>"></i>
                             <?= $property['name'] ?>
                         </li>
                     <?php endforeach; ?>
                 </ul>
             <?php endif; ?>
+            </div>
+
+            <div class="button">
+                <a class="elementor-price-table__button elementor-button elementor-size-md" href="<?= $button_url ?>"> <?= $button_txt ?> </a>
+            </div>
+
         </div>
 
         <?php
+
     }
 
     function form($instance) {
-
+        $title = !empty($instance['title']) ? $instance['title'] : '';
+        $description = !empty($instance['description']) ? $instance['description'] : '';
+        $button_txt = !empty($instance['button_txt']) ? $instance['button_txt'] : '';
+        $button_url = !empty($instance['button_url']) ? $instance['button_url'] : '';
         $content = !empty($instance['content']) ? $instance['content'] : '';
         $properties = !empty($instance['properties']) ? $instance['properties'] : [];
+        $price = !empty($instance['price']) ? $instance['price'] : '';
+        $price_txt = !empty($instance['price_txt']) ? $instance['price_txt'] : '';
+        $color = !empty($instance['color']) ? $instance['color'] : '';
         ?>
         <link rel="stylesheet" id="font-awesome-official-css" href="https://use.fontawesome.com/releases/v6.5.1/css/all.css" media="all">
 
-        <input type="hidden" class="hidden-input-field">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/mdbassit/Coloris@latest/dist/coloris.min.css"/>
+        <script src="https://cdn.jsdelivr.net/gh/mdbassit/Coloris@latest/dist/coloris.min.js"></script>
 
+        <input type="hidden" class="hidden-input-field">
+        <div class="colorscript"></div>
         <div class="elementor-control">
             <details>
                 <summary>Header</summary>
@@ -60,6 +84,24 @@ class Price_Table_Widget extends WC_Widget {
                 <p>
                     <label for="<?php echo $this->get_field_id('description'); ?>"><?php _e('Description:', 'best-shop'); ?></label>
                     <input class="widefat" type="text" id="<?php echo $this->get_field_id('description'); ?>" name="<?php echo $this->get_field_name('description'); ?>" value="<?php echo esc_attr($description); ?>">
+                </p>
+
+                <p>
+                    <label for="color">Choose a color:</label>
+                    <input data-jscolor="{}" type="text" class="color-picker" id="<?php echo $this->get_field_id('description'); ?>"   name="<?php echo $this->get_field_name('color'); ?>"  value="<?php echo esc_attr($color); ?>">
+                </p>
+
+            </details>
+<!--            price!-->
+            <details>
+                <summary>Price</summary>
+                <p>
+                    <label for="<?php echo $this->get_field_id('price'); ?>"><?php _e('Price:', 'best-shop'); ?></label>
+                    <input class="widefat" type="text" id="<?php echo $this->get_field_id('price'); ?>" name="<?php echo $this->get_field_name('price'); ?>" value="<?php echo esc_attr($price); ?>">
+                </p>
+                <p>
+                    <label for="<?php echo $this->get_field_id('price_txt'); ?>"><?php _e('Price text:', 'best-shop'); ?></label>
+                    <input class="widefat" type="text" id="<?php echo $this->get_field_id('price_txt'); ?>" name="<?php echo $this->get_field_name('price_txt'); ?>" value="<?php echo esc_attr($price_txt); ?>">
                 </p>
             </details>
 
@@ -77,17 +119,18 @@ class Price_Table_Widget extends WC_Widget {
 
                             $name = esc_attr($property['name']);
                             $icon = esc_attr($property['icon']);
+                            $color = esc_attr($property['color']);
                             ?>
                             <script>
                                 jQuery(document).ready(function($) {
-                                    addProp('<?php echo $field; ?>', '<?php echo $index; ?>', '<?php echo $name; ?>', '<?php echo $icon; ?>');
+                                    addProp('<?php echo $field; ?>', '<?php echo $index; ?>', '<?php echo $name; ?>', '<?php echo $icon; ?>', '<?php echo $color; ?>' );
                                 });
                             </script>
                         <?php endforeach; ?>
                     <?php else : ?>
                         <script>
                             jQuery(document).ready(function($) {
-                                addProp('<?php echo $field; ?>', '0', '', '');
+                                addProp('<?php echo $field; ?>', '0', '', '', '');
                             });
                         </script>
                     <?php endif; ?>
@@ -95,12 +138,24 @@ class Price_Table_Widget extends WC_Widget {
                 
                 <br/>
                 <button class="elementor-button elementor-repeater-add"><?php _e('Add Property', 'best-shop'); ?></button>
+            </details>
 
+            <details>
+                <summary>Button</summary>
+                <p>
+                    <label for="<?php echo $this->get_field_id('button_txt'); ?>"><?php _e('Text:', 'best-shop'); ?></label>
+                    <input class="widefat" type="text" id="<?php echo $this->get_field_id('button_txt'); ?>" name="<?php echo $this->get_field_name('button_txt'); ?>" value="<?php echo esc_attr($button_txt); ?>">
+                </p>
+                <p>
+                    <label for="<?php echo $this->get_field_id('button_url'); ?>"><?php _e('Url:', 'best-shop'); ?></label>
+                    <input class="widefat" type="text" id="<?php echo $this->get_field_id('button_url'); ?>" name="<?php echo $this->get_field_name('button_url'); ?>" value="<?php echo esc_attr($button_url); ?>">
+                </p>
             </details>
         </div>
 
         <script>
             jQuery(document).ready(function($) {
+
                 $('.<?php echo $randomClassId; ?>').sortable({
                     items: ".elementor-repeater-field",
                     handle: ".sorthandle",
@@ -115,6 +170,7 @@ class Price_Table_Widget extends WC_Widget {
                         wrapper.find('.elementor-repeater-field').each(function(index) {
                             $(this).find('.name').attr('name', '<?php echo $field; ?>[' + index + '][name]');
                             $(this).find('.icon').attr('name', '<?php echo $field; ?>[' + index + '][icon]');
+                            $(this).find('.color').attr('name', '<?php echo $field; ?>[' + index + '][color]');
                         });
 
                         // trigger change event to let elementor know that the field has changed, the preview will then be updated
@@ -123,7 +179,7 @@ class Price_Table_Widget extends WC_Widget {
                 });
             });
 
-            function addProp(field, index, name, icon) {
+            function addProp(field, index, name, icon, color) {
                 // alert('addProp');
                 var propRow = $(`<div class="elementor-repeater-field" style="padding: 10px 5px; border: 2px solid white; margin: 5px 0; position: relative;">
                         <span class="elementor-repeater-delete" style="position: absolute; top: 8px; left: 8px;"><i style="font-size: 18px;" class="fa fa-close"></i></span>
@@ -131,11 +187,13 @@ class Price_Table_Widget extends WC_Widget {
                         <br/>
                         <span style="width: 28%; display: inline-block;">Name:</span><input style="width: 70%;" type="text" attr-id="`+index+`" class="widefat name" name="`+field+`[`+index+`][name]" value="`+name+`" placeholder="Property Name">
                         <span style="width: 28%; display: inline-block;">Icon:</span><input  style="width: 70%;" type="text" class="icon indexfield" name="`+field+`[`+index+`][icon]" value="`+icon+`">
+                        <span style="width: 28%; display: inline-block;">Color:</span><input data-jscolor="{}" style="width: 70%;" type="text" class="color indexfield" name="`+field+`[`+index+`][color]" value="`+color+`">
                     </div>`);
 
                 //var wrapper = field.closest('.elementor-control').find('.elementor-repeater-fields-wrapper');
                 var wrapper = $('.<?= $randomClassId; ?>');
                 wrapper.append(propRow);
+                $('.colorscript').html(`<script src="/wp-includes/js/jscolor.js"/>`);
             }
 
             function determineNextIndex() {
@@ -164,22 +222,11 @@ class Price_Table_Widget extends WC_Widget {
                     $('.hidden-input-field').trigger('change');
                 });
 
-
                 $(document).off('click', '.elementor-repeater-add');
                 $(document).on('click', '.elementor-repeater-add', function(e) {
                     e.preventDefault();
-
                     var nextIndex = determineNextIndex() + 1;
-
-                    alert('next index is ' + nextIndex);
-
                     addProp('<?php echo $this->get_field_name('properties'); ?>', nextIndex, '', '');
-
-                    // var $repeaterWrapper = $(this).closest('.elementor-control').find('.elementor-repeater-fields-wrapper');
-                    // var $template = $repeaterWrapper.find('.elementor-repeater-field').first().clone();
-                    // $template.find('input').val('');
-                    // $repeaterWrapper.append($template);
-                    // $repeaterWrapper.find('.elementor-repeater-remove-btn').show();
                 });
 
                 $(document).off('click', '.elementor-repeater-remove-btn');
@@ -202,18 +249,25 @@ class Price_Table_Widget extends WC_Widget {
         $old_instance['properties'] = !empty($new_instance['properties']) ? $new_instance['properties'] : [];
         $old_instance['title'] = sanitize_text_field($new_instance['title']);
         $old_instance['description'] = sanitize_text_field($new_instance['description']);
+        $old_instance['button_txt'] = sanitize_text_field($new_instance['button_txt']);
+        $old_instance['button_url'] = sanitize_text_field($new_instance['button_url']);
+        $old_instance['price'] = sanitize_text_field($new_instance['price']);
+        $old_instance['price_txt'] = sanitize_text_field($new_instance['price_txt']);
+        $old_instance['color'] = sanitize_text_field($new_instance['color']);
         return $old_instance;
     }
 }
+
+
+
+add_action('widgets_init', function() {
+    register_widget('Price_Table_Widget');
+});
 
 function enqueue_jquery_ui() {
     wp_enqueue_script('jquery-ui-sortable');
 }
 add_action('admin_enqueue_scripts', 'enqueue_jquery_ui');
-
-add_action('widgets_init', function() {
-    register_widget('Price_Table_Widget');
-});
 
 //register the plugin's style.css in both backend and frontend
 add_action('wp_enqueue_scripts', function() {
@@ -223,5 +277,6 @@ add_action('wp_enqueue_scripts', function() {
 add_action('admin_enqueue_scripts', function() {
     $thisDir = str_replace(ABSPATH, '/', dirname(__FILE__));
     wp_enqueue_style('ryxon_widget_style', $thisDir.'/style.css', array(), time(), 'all');
-    wp_enqueue_style('font-awesome', 'https://use.fontawesome.com/releases/v6.5.1/css/all.css', array(), '6.5.1', 'all');
+    wp_enqueue_style('font-awesome', '/wp-includes/css/font-awesome.css', array(), '6.5.1', 'all');
+    wp_enqueue_script('jscolor', '/wp-includes/js/jscolor.js', array(), '1.0', 'all');
 });
